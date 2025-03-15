f944fe4c2db0a75bf46753596b324d419cce12f1
himu004 <himu.chowdhury01@gmail.com>
2025-03-15 13:38:35 +0600

    Movie App Project Added

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-red-300">Welcome to this move App with JSM</Text>
+        <Text className="font-bold text-accent">This is Me</Text>
+    </View>
+  );
+}
+
